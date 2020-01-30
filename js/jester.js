eeb132b0c755fe056a8407d548c09babd059991e
dosisod <39638017+dosisod@users.noreply.github.com>
2020-01-29 22:32:14 -0800

    added pass fail display functionality

--- js/jester.js
+++ js/jester.js
@@ -1,3 +1,73 @@
+const jester={
+
+Test: class {
+	pass=false
+	fail=false
+},
+
+run: function(tests) {
+	var total=new Fraction(tests.length)
+	var fails=new Fraction()
+	var passes=new Fraction()
+
+	nu("total-tests").innerText=""
+	nu("passed-tests").innerText=""
+	nu("failed-tests").innerText=""
+
+	console.log(tests)
+	console.log(tests[0].pass)
+	console.log(tests[0].fail)
+
+	for (const i in tests) {
+		if (tests[i].pass) {
+			passes.addBoth()
+			fails.addBottom()
+		}
+		else if (tests[i].fail) {
+			fails.addBoth()
+			passes.addBottom()
+		}
+
+		total.addTop()
+		updateFraction(total, "total-tests")
+
+		updateFraction(passes, "passed-tests")
+		updateFraction(fails, "failed-tests")
+	}
+}
+
+}
+
+class Fraction {
+	numerator=0
+	denomenator=0
+
+	constructor(denomenator) {
+		this.denomenator=denomenator || 0
+	}
+
+	addBoth() {
+		this.addTop()
+		this.addBottom()
+	}
+
+	addTop() {
+		this.numerator++
+	}
+
+	addBottom() {
+		this.denomenator++
+	}
+}
+
+function updateFraction(fraction, id) {
+	document.getElementById(id).innerText=(
+		fraction.numerator +
+		"/" +
+		fraction.denomenator
+	)
+}
+
 function toggleError(element) {
 	const errorBox=element
 		.parentElement
